d94af4005031b51bf48bf3696fdf66ddf1c57399
gianniskokkosis <gkokkosis1998@gmail.com>
2020-03-25 19:45:31 +0200

    Create parent module seip2020_practical_assignments

---
+++ seip2020_practical_assignments/src/main/java/gr/aueb/dmst/GiannisKokkosis/App.java
@@ -0,0 +1,13 @@
+package gr.aueb.dmst.GiannisKokkosis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
