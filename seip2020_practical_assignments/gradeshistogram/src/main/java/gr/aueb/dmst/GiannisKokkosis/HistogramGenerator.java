06c749944d21b9a7ba9a23c56abbc1dd75d4f6d3
gianniskokkosis <gkokkosis1998@gmail.com>
2020-03-26 00:55:53 +0200

    Create class HistogramGenerator.java in the src foler of the child module

---
+++ seip2020_practical_assignments/gradeshistogram/src/main/java/gr/aueb/dmst/GiannisKokkosis/HistogramGenerator.java
@@ -0,0 +1,16 @@
+package gr.aueb.dmst.GiannisKokkosis;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class HistogramGenerator{
+
+    public void generateChart(int [] grades) {
+        
+
+    }
+}
