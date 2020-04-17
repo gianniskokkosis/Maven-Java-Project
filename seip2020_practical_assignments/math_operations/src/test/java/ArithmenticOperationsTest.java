debb838038d4930378d87fc283518230d7a965e3
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-17 11:08:54 +0300

    Add ArithmeticOperationsTest class
    -ArithmeticOperationsTest class provides simple test cases for the ArithmeticOperations class
    through unit tests

---
+++ seip2020_practical_assignments/math_operations/src/test/java/ArithmenticOperationsTest.java
@@ -0,0 +1,14 @@
+package math
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+* The ArithmeticOperationsTest class provides all the necessary 
+* unit tests and test cases for ArithmeticOperations class 
+*
+* @author gianniskokkosis
+*/
+public class ArithmeticOperationsTest{
+
+}
