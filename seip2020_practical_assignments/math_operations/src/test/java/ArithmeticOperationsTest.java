5e697dc56a8c60c8726163ee1fa2778ac1ef71a0
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-17 21:50:58 +0300

    Fix typo on ArithmeticOperationsTest class
    -Fix typo in line 5 of ArithmeticOperationsTest

--- seip2020_practical_assignments/math_operations/src/test/java/ArithmeticOperationsTest.java
+++ seip2020_practical_assignments/math_operations/src/test/java/ArithmeticOperationsTest.java
@@ -2,7 +2,7 @@ package math;
 
 import org.junit.Test;
 import org.junit.Assert;
-import org.junit.rules.ExcpectedException;
+import org.junit.rules.ExpectedException;
 import org.junit.Rules;
 
 /**
