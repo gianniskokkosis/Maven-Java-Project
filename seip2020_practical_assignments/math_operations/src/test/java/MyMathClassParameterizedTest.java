90b9983d7715f5a589958f6138c926401cdf2564
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-18 12:52:37 +0300

    Update MyMathClassParameterizedTest class
    -Add parameters and expected results in order to run the parameterised test.

--- seip2020_practical_assignments/math_operations/src/test/java/MyMathClassParameterizedTest.java
+++ seip2020_practical_assignments/math_operations/src/test/java/MyMathClassParameterizedTest.java
@@ -9,7 +9,23 @@ import org.junit.runner.Parameterized;
 import org.junit.runner.Parameterized.Parameter;
 import org.junit.runner.Parameterized.Parameters;
 
+import org.junit.Assert;
+
 @RunWith(Parameterized.class)
 public class MyMathClassParameterizedTest{
 
+    MyMath mym = new MyMath();
+
+    @Parameter (value = 0)
+    public int number = 0;
+
+    @Parameter (value = 1)
+    public int result;
+
+    @Parameters
+    public static Collection<Object[]> data(){
+        Object[][] data = new Object[][]{{1, 1}, {2, 2}, {12, 479001600}};
+
+        return Arrays.asList(data);
+    }
 }
