1aaca9cd485fe1fdc1e7e109807acecc13e9b8eb
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-18 12:41:20 +0300

    Update MyMathClassParameterizedTest class
    -Add proper imports in order to run Parameterized test correct

--- seip2020_practical_assignments/math_operations/src/test/java/MyMathClassParameterizedTest.java
+++ seip2020_practical_assignments/math_operations/src/test/java/MyMathClassParameterizedTest.java
@@ -1,5 +1,14 @@
 package math;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Parameterized;
+import org.junit.runner.Parameterized.Parameter;
+import org.junit.runner.Parameterized.Parameters;
+
 @RunWith(Parameterized.class)
 public class MyMathClassParameterizedTest{
 
