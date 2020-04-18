8b81809a8ff59f69b6a5f42036c451d5e52ace68
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-18 12:07:29 +0300

    Update MyMath class
    -Implement factorial's method body. Now this method will calculate the factorial of a number wich is between 0 and 12

--- seip2020_practical_assignments/math_operations/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/math_operations/src/main/java/math/MyMath.java
@@ -1,8 +1,18 @@
-package math
+package math;
 
 public class MyMath{
     
     public int factorial(int n){
-        
+        int f = 1;
+
+        if (n < 0 || n > 12){
+            throw new IllegalArgumentException("Wrong! Number must be greater than 0 and smaller than 12. ");
+        } else {
+            for (int i = 1; i <= n; i++){
+                f *= 1;
+            }
+        }
+
+        return f;
     }
 }
