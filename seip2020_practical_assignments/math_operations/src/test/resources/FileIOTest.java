c24f36d8af6e8faaea1d5ce43a5378a3c6a24938
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-19 23:04:29 +0300

    Update FileIOTest test class
    -Add testReadFileValidInput test case in FileIOTest class in order to test if the method readFile of FileIO class reads a file with valid values.

--- seip2020_practical_assignments/math_operations/src/test/resources/FileIOTest.java
+++ seip2020_practical_assignments/math_operations/src/test/resources/FileIOTest.java
@@ -5,5 +5,19 @@ import org.junit.Assert;
 
 
 public class FileIOTest {
+
+    FileIO file = new FileIO();
+    public static String resourcesPath = "src/test/resources/";
+
+    @Test
+    public void testReadFileValidInput(){
+
+         int[] excpectedValues = new int[] {
+            1, 2, 3, 4, 10, 12, 22, 3333, 12,
+            98, 100, 99, 39876, 343, 0, 1, 4};
+
+        String validInputFilePath = resources.concat("valid_values.txt");
+        Assert.assertArrayEquals(excpectedValues, file.readFile(validInputFilePath));
+    }
     
 }
