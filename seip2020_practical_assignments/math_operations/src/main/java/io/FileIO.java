249196bc06de866c46abac6bcb4297084bdc5b20
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-20 19:10:45 +0300

    Upadte FileIO class
    -Modify readFile code to ignore lines of the file if they are not represent intgeres numbers.

--- seip2020_practical_assignments/math_operations/src/main/java/io/FileIO.java
+++ seip2020_practical_assignments/math_operations/src/main/java/io/FileIO.java
@@ -38,8 +38,14 @@ public class FileIO {
 			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				int number = Integer.parseInt(line);
-				numbersList.add(number);
+
+				if (isInt(line)){
+					int number = Integer.parseInt(line);
+					numbersList.add(number);
+				}else{
+					continue;
+				}
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
