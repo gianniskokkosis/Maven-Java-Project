04c141ec54f1f84928f8801d560d868ebae9c2dc
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-21 00:26:35 +0300

    Update ArrayOperations class
    -Implement findPrimesInFile method which finds all the prime numbers form a file and stores them in an ArrayList.

--- seip2020_practical_assignments/math_operations/src/main/java/math/ArrayOperations.java
+++ seip2020_practical_assignments/math_operations/src/main/java/math/ArrayOperations.java
@@ -1,9 +1,25 @@
 package math;
 import io.FileIO;
+import java.util.ArrayList;
 
 public class ArrayOperations {
 
     FileIO fileio = new FileIO();
     MyMath myMath = new MyMath();
+
+    public ArrayList<Integer> findPrimesInFile(FileIO fileio, String filepath, 
+        MyMath myMath){
+
+        int array [] = fileio.readFile(filepath);
+        ArrayList<Integer> primeArrayList = new ArrayList<Integer>();
+
+        for (int i = 0; i < array.length(); i++){
+            if (myMath.isPrime(array[i])){
+                primeArrayList.add(array[i]);
+            }
+        }
+
+        return primeArrayList;
+    }
     
 }
