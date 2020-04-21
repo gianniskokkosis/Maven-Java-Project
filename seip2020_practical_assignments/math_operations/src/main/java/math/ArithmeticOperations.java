01c538be7a0f13db9e59c334779f41e9230a46ad
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-22 00:22:01 +0300

    Update ArithmeticOperations class
    -Fix logical erros in multiply method body

--- seip2020_practical_assignments/math_operations/src/main/java/math/ArithmeticOperations.java
+++ seip2020_practical_assignments/math_operations/src/main/java/math/ArithmeticOperations.java
@@ -36,10 +36,10 @@ public class ArithmeticOperations {
 	public int multiply(int x, int y) {
         if (x < 0 || y < 0) {
             throw new IllegalArgumentException("x & y should be >= 0");
-        } else if (y == 0){
-            return x*y; 
+        } else if (x <= Integer.MAX_VALUE && y == 0){
+            return 0; 
         }else if (x <= Integer.MAX_VALUE/y){  
-            return 0;
+            return x*y;
         } else {
             throw new IllegalArgumentException("The product does not fit in an Integer variable");      
         }
