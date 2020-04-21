a31bbbc0b74d48cda57fc1cdc64e0350568ae809
gianniskokkosis <gkokkosis1998@gmail.com>
2020-04-21 11:43:37 +0300

    Update ArrayOperationsTest test class
    -Add test case findPrimesInFileMocking in order to test if findPrimesInFile method of ArrayOperations class is returning an array that contains only prime numbers.

--- seip2020_practical_assignments/math_operations/src/test/java/ArrayOperationsTest.java
+++ seip2020_practical_assignments/math_operations/src/test/java/ArrayOperationsTest.java
@@ -6,5 +6,36 @@ import org.junit.Assert;
 import static org.mockito.Mockito.*;
 
 public class ArrayOperationsTest {
-    
+
+    public static String resourcesPath = "src/test/resources/";
+
+    @Test
+    public void findPrimesInFileMocking(){
+
+        ArrayOperations arrop = new ArrayOperations();
+
+        MyMath mm = mock(MyMath.class);
+        FileIO fio = mock(FileIO.class);
+        String valid = resourcesPath.concat("test_mock.txt");
+
+        int[] excpectedValues = new int[] {
+            3, 4, 5, 6, 7, 8, 9, 10, 100, 200, 150};
+
+        when(fio.readFile(valid).thenReturn(excpectedValues));
+
+        when(mm.isPrime(3).thenReturn(true));
+        when(mm.isPrime(4).thenReturn(false));
+        when(mm.isPrime(5).thenReturn(true));
+        when(mm.isPrime(6).thenReturn(false));
+        when(mm.isPrime(7).thenReturn(true));
+        when(mm.isPrime(8).thenReturn(false));
+        when(mm.isPrime(9).thenReturn(false));
+        when(mm.isPrime(10).thenReturn(false));
+        when(mm.isPrime(100).thenReturn(false));
+        when(mm.isPrime(200).thenReturn(false));
+        when(mm.isPrime(150).thenReturn(false));
+
+        Assert.assertArrayEquals(new int[]{3, 5, 7},
+            arrop.findPrimesInFile(fio, valid, mm));
+    }
 }
