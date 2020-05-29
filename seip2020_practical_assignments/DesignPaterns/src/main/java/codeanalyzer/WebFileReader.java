6d7457eb82e615dd345248a1dd501d4b74abbb82
gianniskokkosis <t8160190@aueb.gr>
2020-05-29 20:00:01 +0300

    Add WebFileReader class
    - WebFileReader class reads a java source code file from the web and stores its contents in a List or a String.
    
    - This addition reduces the average complexity of the class and makes more clear what is the exact functionality of the class.

---
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/WebFileReader.java
@@ -0,0 +1,41 @@
+package codeanalyzer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WebFileReader implements SourceFileReader{
+
+    @Override
+    public List<String> readFileIntoString(String filepath) throws IOException{
+        List<String> lines = new ArrayList<>();
+        URL url = new URL(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        return lines;
+    }
+
+    @Override
+    public Strign readFileIntoString(String filepath) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        File file = new File(filepath);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+}
