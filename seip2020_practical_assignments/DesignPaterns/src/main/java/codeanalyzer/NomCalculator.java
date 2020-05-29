f453f40bae3a4da689a38bd20feb3c020a61abdd
gianniskokkosis <t8160190@aueb.gr>
2020-05-30 00:28:59 +0300

    Update NomCalculator class
    - Implement calculateMetric methods body of NomCalculator class
    
    - This addition will allow NomCalculator class to calculate NOM metric for a java source code file, through calculateMetric method

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/NomCalculator.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/NomCalculator.java
@@ -3,6 +3,28 @@ package codeanalyzer;
 public class NomCalculator implements MetricCalculator{
 
     public int calculateMetric(String filepath, String analyzerType){
-        //do something here
+        if(analyzerType.equals("regex")) {
+            String sourceCode = fileReader.readFileIntoString(filepath);
+            Pattern pattern = Pattern.compile(".*(public |protected |private |static )?[\\w\\<\\>\\[\\]]+\\s+(\\w+) *\\([^\\)]*\\) *(\\{?|[^;]).*"); 
+            Matcher methodSignatures = pattern.matcher(sourceCode);
+
+            int methodCounter = 0;
+            while (methodSignatures.find()) {
+                methodCounter++;
+            }
+            return methodCounter;
+		} else if (analyzerType.equals("strcomp")) {
+            List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+            int methodCounter = 0;
+            for (String line : sourceCodeList) {
+                line = line.stripLeading().stripTrailing(); //clear all leading and trailing white spaces
+                if ( ((line.contains("public") || line.contains("private") || line.contains("protected"))
+                        || line.contains("void") || line.contains("int") || line.contains("String"))
+                    && line.contains("(") && line.contains(")") && line.contains("{") )
+                    methodCounter++;
+            }
+            return methodCounter; 
+		}
+        return -1;
     }
 }
