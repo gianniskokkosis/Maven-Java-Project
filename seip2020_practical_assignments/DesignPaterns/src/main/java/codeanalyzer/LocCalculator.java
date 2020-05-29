ca63574f1a35cb1df5a93f4b6e74ca0c41864013
gianniskokkosis <t8160190@aueb.gr>
2020-05-30 00:25:01 +0300

    Update LocCalculator class
    - Implement calculateMetric methods body of LocCalculator class
    
    - This addition will allow LocCalculator class to calculate LOC metric for a java source code file, through calculateMetric method

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/LocCalculator.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/LocCalculator.java
@@ -2,7 +2,33 @@ package codeanalyzer;
 
 public class LocCalculator implements MetricCalculator{
 
+    @Override
     public int calculateMetric(String filepath, String analyzerType){
-        //do something here
+        if(analyzerType.equals("regex")) {
+            String sourceCode = fileReader.readFileIntoString(filepath);
+            Pattern pattern = Pattern.compile("((//.*)|(/\\*.*)|(\\*+.*))");
+            Matcher nonCodeLinesMatcher = pattern.matcher(sourceCode);
+
+            int nonCodeLinesCounter = 0;
+            while (nonCodeLinesMatcher.find()) {
+                nonCodeLinesCounter++;
+            }
+
+            int sourceFileLength = sourceCode.split("\n").length;
+            int loc =  sourceFileLength - nonCodeLinesCounter;
+
+            return loc;
+		} else if (analyzerType.equals("strcomp")) {
+            List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+            int nonCodeLinesCounter = 0;
+            for (String line : sourceCodeList) {
+                line = line.stripLeading().stripTrailing(); //clear all leading and trailing white spaces
+                if (line.startsWith("//") || line.startsWith("/*") || line.startsWith("*") || line.equals("{") || line.equals("}") || line.equals(""))
+                    nonCodeLinesCounter++;
+            }
+            int loc = sourceCodeList.size() - nonCodeLinesCounter;
+            return loc; 
+		}
+        return -1;
     }
 }
