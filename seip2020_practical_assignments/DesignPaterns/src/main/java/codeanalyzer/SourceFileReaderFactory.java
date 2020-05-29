e4f7e0c884184107fd2de7fac1235242ab57bbaf
gianniskokkosis <t8160190@aueb.gr>
2020-05-29 23:46:52 +0300

    Add getSourceFileReader method in SourceFileReaderFactory class
    - This method creates the right object of Source File Reader
    
    - If the user wants to analyze code from the web then the created source file reader will be an instance of WebFileReader class.
    
    - If the user wants to analyze code from a local java source code file then the created source file reader will be an instance of LocalFileReader class.

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReaderFactory.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReaderFactory.java
@@ -1,5 +1,21 @@
 package codeanalyzer;
 
 public class SourceFileReaderFactory {
-    
+
+    public SourceFileReader getSourceFileReader(String fileReaderType)
+        throws IllegalArgumentExceptions{
+        SourceFileReader reader;
+
+        if (fileReaderType.equals("web")){
+            reader = new WebFileReader();
+        }else if (fileReaderType.equals("local")){
+            reader = new LocalFileReader();
+        }else{
+            throw new IllegalArgumentException(
+                "Unknown type " + fileReaderType
+            );
+        }
+
+        return reader;
+    }
 }
