811607e68f7afa645fc88e8ba7fb6a9a940b0d54
gianniskokkosis <t8160190@aueb.gr>
2020-05-30 00:32:48 +0300

    Update NocCalculator class
    - Implement calculateMetric methods body of NocCalculator class
    
    - This addition will allow NocCalculator class to calculate NOC metric for a java source code file, through calculateMetric method

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/NocCalculator.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/NocCalculator.java
@@ -3,6 +3,27 @@ package codeanalyzer;
 public class NocCalculator implements MetricCalculator{
 
     public int calculateMetric(String filepath, String analyzerType){
-        //do something here
+		if(analyzerType.equals("regex")) {
+            String sourceCode = fileReader.readFileIntoString(filepath);
+            Pattern pattern = Pattern.compile(".*\\s*class\\s+.*"); 
+            Matcher classSignatures = pattern.matcher(sourceCode);
+
+            int classCounter = 0;
+            while (classSignatures.find()) {
+                classCounter++;
+            }
+            return classCounter;
+		} else if (analyzerType.equals("strcomp")) {
+            List<String> sourceCodeList = fileReader.readFileIntoList(filepath);
+            int classCounter = 0;
+            for (String line : sourceCodeList) {
+                line = line.strip(); //remove leading and trailing white spaces
+                if ((line.startsWith("class ") || line.contains(" class ")) && line.contains("{")) {
+                    classCounter++;
+                }
+            }
+            return classCounter; 
+		}
+        return -1;
     }
 }
