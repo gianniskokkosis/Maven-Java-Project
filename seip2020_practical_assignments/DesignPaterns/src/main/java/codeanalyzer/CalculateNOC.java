4612581773e26efdfdf2c7b6908a9a9bb3fee01d
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 10:33:11 +0300

    Update CalculateNOC class
    - Add CalculateNOC contructor.
    
    - With this addtion every object of CalculateNOC class will instantiate the right SourceFileReader object in order to read the java source code file either from web or local.
    
    - Add throw IOException statement in calculateMetric method becaause readFileIntoString and readFileIntoList methods are throwing an IOException.

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/CalculateNOC.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/CalculateNOC.java
@@ -1,9 +1,20 @@
 package codeanalyzer;
 
-public class NocCalculator implements SourceCodeAnalyzer{
+import java.io.IOException;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalculateNOC implements SourceCodeAnalyzer{
+
+    public CalculateNOC(SourceFileReader fileReader) {
+		super(fileReader);
+	}
+    
     @Override
-    public int calculateMetric(String filepath, String analyzerType){
+    public int calculateMetric(String filepath, String analyzerType)
+            throws IOException{
 		if(analyzerType.equals("regex")) {
             String sourceCode = fileReader.readFileIntoString(filepath);
             Pattern pattern = Pattern.compile(".*\\s*class\\s+.*"); 
