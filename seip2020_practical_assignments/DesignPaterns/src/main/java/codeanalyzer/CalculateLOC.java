f88d582bf437debc7038fb4ffe7435098d5a6d74
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 10:19:46 +0300

    Update CalculateLOC class
    - Add CalculateLOC contructor.
    
    - With this addtion every object of CalculateLOC class will instantiate the right SourceFileReader object in order to read the java source code file either from web or local.
    
    - Add throw IOException statement in calculateMetric method becaause readFileIntoString and readFileIntoList methods are throwing an IOException

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/CalculateLOC.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/CalculateLOC.java
@@ -1,14 +1,20 @@
 package codeanalyzer;
 
+import java.io.IOException;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CalculateLOC implements SourceCodeAnalyzer{
 
-	public CalculateLOC(CodeFileReader fileReader) {
+    public CalculateLOC(SourceFileReader fileReader) {
 		super(fileReader);
 	}
-	
 
     @Override
-    public int calculateMetric(String filepath, String analyzerType){
+    public int calculateMetric(String filepath, String analyzerType) 
+            throws IOException{
         if(analyzerType.equals("regex")) {
             String sourceCode = fileReader.readFileIntoString(filepath);
             Pattern pattern = Pattern.compile("((//.*)|(/\\*.*)|(\\*+.*))");
