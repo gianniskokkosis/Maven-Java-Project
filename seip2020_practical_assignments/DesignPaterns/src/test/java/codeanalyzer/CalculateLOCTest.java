e0c7635a4fd49169de58f0fedb9d7b8c4a206c54
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 20:20:48 +0300

    Add CalculateLOCTest test class
    - This class will execute various test cases for the CalculateLOC class.

---
+++ seip2020_practical_assignments/DesignPaterns/src/test/java/codeanalyzer/CalculateLOCTest.java
@@ -0,0 +1,20 @@
+package codeanalyzer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import codeanalyzer.SourceCodeAnalyzer;
+
+import static org.junit.Assert.*;
+
+public class CalculateLOCTest{
+    
+}
