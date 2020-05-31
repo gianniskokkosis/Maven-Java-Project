3798c5582c07a6307b2661237a2bd8f9a4bd6f00
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 20:55:24 +0300

    Add testCalculateRegexLocalLOC test case
    - This test case tests if LOC metric of TestClass is equal to 21.

--- seip2020_practical_assignments/DesignPaterns/src/test/java/codeanalyzer/CalculateLOCTest.java
+++ seip2020_practical_assignments/DesignPaterns/src/test/java/codeanalyzer/CalculateLOCTest.java
@@ -11,10 +11,19 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import codeanalyzer.SourceCodeAnalyzer;
+import codeanalyzer.*;
 
 import static org.junit.Assert.*;
 
 public class CalculateLOCTest{
-    
+    private static List<String> testSourceCode;
+	private final static String TYPE_REGEX = "regex";
+	private final static String TYPE_STRCOMP = "strcomp";
+	private final static String TEST_CLASS = "src/test/resources/TestClass.java";
+	SourceCodeAnalyzer scal = new CalculateLOC(new LocalFileReader());
+
+    @Test
+    public void testCalculateRegexLocalLOC() throws IOException{
+        assertEquals(21, scal.clalcuteMetric(TEST_CLASS, TYPE_REGEX));
+    }
 }
