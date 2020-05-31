94d370d390e139d40e56009d6ee9a12cb878df6b
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 11:21:40 +0300

    Add SourceCodeAnalyzerFactory class
    - This class is responsible for the instantiation of all the different SourceCodeAnalyzer objects.
    
    - The objects will be created through createSourceCodeAnalyzerObjs method wich takes analyzerType(regex or strcomp) and sourceFileLocation (web or local) as arguments.
    
    - If the user gives web as sourceFileLocation then the above method will create three SourceCodeAnalyzer objects(LOC, NOM, NOC) with WebFileReader object as SourceFileReader.
    
    - If the user gives local as sourceFileLocation then the above method will create the same SourceCodeanalyzer objects(LOC, NOM, NOC) with LocalFileReader object as SourceFileReader.

---
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceCodeAnalyzerFactory.java
@@ -0,0 +1,13 @@
+package codeanalyzer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class SourceCodeAnalyzerFactory{
+
+    public List<SourceCodeAnalyzer> createSourceCodeAnalyzerObjs(String analyzerType, 
+            String sourceFileLocation){
+
+    }
+}
