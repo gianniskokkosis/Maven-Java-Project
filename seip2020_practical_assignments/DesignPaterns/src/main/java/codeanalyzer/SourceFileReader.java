98a03572e68a2d8ce44a3d92f4ae4269db118925
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 11:05:35 +0300

    Remove unecessary imports from SourceFileReader interface

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReader.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReader.java
@@ -1,14 +1,6 @@
 package codeanalyzer;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Retrieves (reads) the contents of a given file.
  * The file can be stored locally or exist on the web.
