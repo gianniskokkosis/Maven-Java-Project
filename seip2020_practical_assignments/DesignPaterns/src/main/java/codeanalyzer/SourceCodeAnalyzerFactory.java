23c6f17239870dbf39597a3e6df3ab83a39b2eea
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 11:37:24 +0300

    Implement createSourceCodeAnalyzerObjs method body of SourceCodeAnalyzerFactory class
    - This method creates the right SourceCodeAnalyzer objects with the right SourceFileReader object based on the sourceFileLocation argument.

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceCodeAnalyzerFactory.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceCodeAnalyzerFactory.java
@@ -9,5 +9,25 @@ public class SourceCodeAnalyzerFactory{
     public List<SourceCodeAnalyzer> createSourceCodeAnalyzerObjs(String analyzerType, 
             String sourceFileLocation){
 
+        List<SourceCodeAnalyzer> objs = new ArrayList<SourceCodeAnalyzer>();
+		SourceCodeAnalyzer analyzeLoc = null;
+		SourceCodeAnalyzer analyzeNoc = null;
+		SourceCodeAnalyzer analyzeNom = null;
+		if (sourceFileLocation.equals("web")) {
+			analyzeLoc = new CalculateLOC(new WebFileReader());
+			analyzeNoc = new CalculateNOC(new WebFileReader());
+			analyzeNom = new CalculateNOM(new WebFileReader());
+		}else if (sourceFileLocation.equals("local")) {
+			analyzeLoc = new CalculateLOC(new LocalFileReader());
+			analyzeNoc = new CalculateNOC(new LocalFileReader());
+			analyzeNom = new CalculateNOM(new LocalFileReader());
+		}
+		
+		objs.add(analyzeLoc);
+		objs.add(analyzeNoc);
+		objs.add(analyzeNom);
+		
+		return objs;
+
     }
 }
