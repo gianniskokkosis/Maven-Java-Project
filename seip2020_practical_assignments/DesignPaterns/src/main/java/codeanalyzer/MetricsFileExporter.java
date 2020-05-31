fb5bda8514ec33979756ef32198681685e5e8bd9
gianniskokkosis <t8160190@aueb.gr>
2020-05-31 22:20:22 +0300

    Fix and re-organize all the code because it was implementes wrong.

--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/MetricsFileExporter.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/MetricsFileExporter.java
@@ -1,8 +1,48 @@
 package codeanalyzer;
 
 import java.util.Map;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public interface MetricsFileExporter {
 	
 	public void writeFile(String outputType, Map<String, Integer> metrics, String filepath);
 }
+
+class MetricsExporterCSV implements MetricsFileExporter {
+
+	@Override
+	public void writeFile(String outputType, Map<String, Integer> metrics, String filepath) {
+		File outputFile = new File(filepath + ".csv");
+		StringBuilder metricsNames = new StringBuilder();
+		StringBuilder metricsValues = new StringBuilder();
+		
+		for (Map.Entry<String, Integer> entry : metrics.entrySet()) {
+			metricsNames.append(entry.getKey() + ",");
+			metricsValues.append(entry.getValue()+",");
+		}
+		
+		try {
+			FileWriter writer = new FileWriter(outputFile);
+			writer.append(metricsNames + "\n");
+			writer.append(metricsValues + "\n");
+			writer.close();
+			System.out.println("Metrics saved in " + outputFile.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
+class MetricsExporterJSON implements MetricsFileExporter {
+
+	@Override
+	public void writeFile(String outputType, Map<String, Integer> metrics, String filepath) {
+		//do som
+
+	}
+
+}
+
--- seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReader.java
+++ seip2020_practical_assignments/DesignPaterns/src/main/java/codeanalyzer/SourceFileReader.java
@@ -2,6 +2,13 @@ package codeanalyzer;
 
 import java.io.IOException;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.net.URL;
+
 /**
  * Retrieves (reads) the contents of a given file.
  * The file can be stored locally or exist on the web.
@@ -14,4 +21,63 @@ public interface SourceFileReader{
 	public List<String> readFileIntoList(String filepath) throws IOException;
 	
 	public String readFileIntoString(String filepath) throws IOException;
+}
+
+class LocalFileReader implements SourceFileReader{
+
+    @Override
+    public List<String> readFileIntoList(String filepath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        File file = new File(filepath);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+        lines.add(line);
+        }
+        reader.close();
+        return lines;
+    }
+
+    @Override
+    public String readFileIntoString(String filepath) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        File file = new File(filepath);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+        sb.append(line + "\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+}
+
+class WebFileReader implements SourceFileReader{
+
+    @Override
+    public List<String> readFileIntoList(String filepath) throws IOException{
+        List<String> lines = new ArrayList<>();
+        URL url = new URL(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        return lines;
+    }
+
+    @Override
+    public String readFileIntoString(String filepath) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        URL url = new URL(filepath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
 }
